2d560c47e6311802b5bd15fb473ce043650c73da
carmenelenab <bruma.carmenelena@yahoo.com>
2024-05-19 12:41:24 +0200

    List of adjustmens
    *script.js - improvements
    *style.css - adding airplane image
    *style.css - adding background image

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ let airplaneLeft;
 let avoidedObjects = 0;
 let gameOver = false;
 
+const MINIM_WIDTH = 20;
+const VARIATION_WIDTH = 50;
+
+const HOUR = 60;
+
 window.addEventListener('load', () => {
     setInitialPosition();
     startTimer();
@@ -27,7 +32,7 @@ let timerInterval;
 function startTimer() {
     timerInterval = setInterval(() => {
         ++seconds;
-        if (seconds === 60) {
+        if (seconds === HOUR) {
             seconds = 0;
             ++minutes;
         }
@@ -36,22 +41,19 @@ function startTimer() {
 
 //listening the keydown events
 window.addEventListener('keydown', (e) => {
-    switch (e.key) {
-        case 'ArrowLeft':
-            if (airplaneLeft > 0) {
-                airplaneLeft -= moveBy;
-                airplaneLeft = Math.max(airplaneLeft, 0);
-                airplane.style.left = airplaneLeft + 'px';
-            }
-            break;
-        case 'ArrowRight':
-            if (airplaneLeft + airplaneWidth < gameBoardWidth) {
-                airplaneLeft += moveBy;
-                let maxRightPosition = gameBoardWidth - airplaneWidth;
-                airplaneLeft = Math.min(airplaneLeft, maxRightPosition);
-                airplane.style.left = airplaneLeft + 'px';
-            }
-            break;
+    if (e.key === 'ArrowLeft') {
+        if (airplaneLeft > 0) {
+            airplaneLeft -= moveBy;
+            airplaneLeft = Math.max(airplaneLeft, 0);
+            airplane.style.left = airplaneLeft + 'px';
+        }
+    } else if (e.key === 'ArrowRight') {
+        if (airplaneLeft + airplaneWidth < gameBoardWidth) {
+            airplaneLeft += moveBy;
+            let maxRightPosition = gameBoardWidth - airplaneWidth;
+            airplaneLeft = Math.min(airplaneLeft, maxRightPosition);
+            airplane.style.left = airplaneLeft + 'px';
+        }
     }
 });
 
@@ -63,9 +65,12 @@ function generateFallingObject() {
     let fallingObject = document.createElement('div');
     fallingObject.classList.add('fallingObject');
 
-    // Randomize the width(between 20 and 70 pixels) and position of the falling object
-    let objectWidth = Math.floor(Math.random() * 50) + 20;
-    let objectLeft = Math.floor(Math.random() * (gameBoard.offsetWidth - objectWidth)); // Random left position within the game board
+    // Randomize the width(between 20 and 70 pixels) and position of the 
+    //falling object
+    let objectWidth = Math.floor(Math.random() * VARIATION_WIDTH) + MINIM_WIDTH;
+    // Random left position within the game board
+    let objectLeft =
+        Math.floor(Math.random() * (gameBoard.offsetWidth - objectWidth));
 
     // Apply styles to the falling object
     fallingObject.style.width = objectWidth + 'px';
@@ -83,10 +88,12 @@ function generateFallingObject() {
         let topPosition = parseInt(fallingObject.style.top);
         let bottomPosition = topPosition + fallingObject.offsetHeight;
         if (bottomPosition < gameBoard.offsetHeight) {
-            fallingObject.style.top = (topPosition + 5) + 'px'; // Adjust speed as needed
+            // Adjust speed as needed
+            fallingObject.style.top = (topPosition + 5) + 'px';
         } else {
             clearInterval(fallInterval);
-            gameBoard.removeChild(fallingObject); // Remove the falling object when it reaches the bottom
+            // Remove the falling object when it reaches the bottom
+            gameBoard.removeChild(fallingObject);
             if (!gameOver) {
                 ++avoidedObjects;
                 console.log(avoidedObjects);
@@ -96,13 +103,16 @@ function generateFallingObject() {
 }
 
 // Call function at regular intervals to generate falling objects
-let objectInterval = setInterval(generateFallingObject, 2000); // Adjust interval as need
+// Adjust interval as need
+let objectInterval = setInterval(generateFallingObject, 2000);
 
 function checkCollision() {
-    let airplaneRect = airplane.getBoundingClientRect(); // Get the position and size of the airplane
+    // Get the position and size of the airplane
+    let airplaneRect = airplane.getBoundingClientRect();
     // Loop through all falling objects
     document.querySelectorAll('.fallingObject').forEach((fallingObject) => {
-        let fallingObjectRect = fallingObject.getBoundingClientRect(); // Get the position and size of the falling object
+        // Get the position and size of the falling object
+        let fallingObjectRect = fallingObject.getBoundingClientRect();
 
         // Check for collision
         if (fallingObjectRect.left < airplaneRect.right &&
@@ -123,7 +133,8 @@ setInterval(checkCollision, 100); // Check for collision every 100 milliseconds
 function isGameOver() {
     let score = document.getElementById('score');
     document.getElementById("game-over").style.display = "block";
-    timerElement.textContent = "Time Survived: " + minutes + " minutes " + seconds + " seconds";
+    timerElement.textContent =
+        "Time Survived: " + minutes + " minutes " + seconds + " seconds";
     score.textContent = "Avoided Objects: " + avoidedObjects;
 }
 
